66ffa8c9e4b9e067c6c80365b47f7e54ceec223f
Martin Volek <martin.volek@ext.csas.cz>
2021-08-19 17:03:07 +0200

    remove usings

--- HangfireTest/Startup.cs
+++ HangfireTest/Startup.cs
@@ -1,19 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Hangfire;
 using Hangfire.Console;
-using Hangfire.Dashboard;
 using Hangfire.Pro.Redis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace HangfireTest
 {
